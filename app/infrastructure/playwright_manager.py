a3adda26f1f84224d879db0f5d725173ca859178
jejimeneza <jimenez.ing.sis@gmail.com>
2025-04-30 22:22:14 -0500

    Refactoring. Adding unit test and integration test

--- app/infrastructure/playwright_manager.py
+++ app/infrastructure/playwright_manager.py
@@ -159,6 +159,14 @@ class PlaywrightManager(BrowserManagerInterface):
                 headless=self.config.headless
             )
             self._page = await self._browser.new_page()
+            self._context = await self._browser.new_context(
+                java_script_enabled=True,  # Explicitly enable JavaScript
+                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                #viewport={"width": 1280, "height": 720},
+                locale="en-US",
+                timezone_id="America/New_York",
+                ignore_https_errors=True  # Optional for SSL issues
+            )
             await self._configure_page()
 
             if self.start_url:
